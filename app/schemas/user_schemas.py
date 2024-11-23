c90203fabdd6a8971197d748055eb97dad9dc49c
Jay <jhr4@njit.edu>
2024-11-23 18:08:34 -0500

    changed nickname to be only generated once and used throughout

--- app/schemas/user_schemas.py
+++ app/schemas/user_schemas.py
@@ -22,26 +22,6 @@ def validate_url(url: Optional[str]) -> Optional[str]:
         raise ValueError('Invalid URL format')
     return url
 
-def github_validate_url(url: Optional[str]) -> Optional[str]:
-    if url is None:
-        return url
-    github_url_regex = r'^https?:\/\/(www\.)?github\.com\/[a-zA-Z0-9-_]+\/?$'
-
-    if not re.match(github_url_regex, url):
-        raise ValueError('Invalid GitHub profile URL. It should match: https://github.com/<username>.')
-    
-    return url
-
-def linkedin_validate_url(url: Optional[str]) -> Optional[str]:
-    if url is None:
-        return url
-    linkedin_url_regex = r'^https?:\/\/(www\.)?linkedin\.com\/in\/[a-zA-Z0-9-_]+\/?$'
-
-    if not re.match(linkedin_url_regex, url):
-        raise ValueError('Invalid LinkedIn profile URL. It should match: https://linkedin.com/in/<username>.')
-    
-    return url
-
 def validate_nickname(nickname: Optional[str]) -> Optional[str]:
     if nickname is None:
         return nickname
@@ -55,36 +35,14 @@ def validate_nickname(nickname: Optional[str]) -> Optional[str]:
 
     return nickname
 
-def validate_password(password: str) -> str:
-    if len(password) < 8:
-        raise ValueError("Password must be at least 8 characters long.")
-    
-    has_uppercase = False
-    has_lowercase = False
-    for i in password:
-        if i.isupper():
-            has_uppercase = True
-        if i.islower():
-            has_lowercase = True
-        if has_lowercase and has_uppercase:
-            break
-    
-    if not has_uppercase or not has_lowercase:
-        raise ValueError("Password must contain at least one uppercase and lowercase letter.")
-    
-    has_digit = False
-    for i in password:
-        if i.isdigit():
-            has_digit = True
-            break
-    if not has_digit:
-        raise ValueError("Password must contain at least one number.")
-        
-    return password
+def get_random_nickname() -> str:
+    return generate_nickname()
+
+random_nickname = get_random_nickname()
 
 class UserBase(BaseModel):
     email: EmailStr = Field(..., example="john.doe@example.com")
-    nickname: Optional[str] = Field(None, example=generate_nickname())
+    nickname: Optional[str] = Field(None, example=random_nickname)
     first_name: Optional[str] = Field(None, example="John")
     last_name: Optional[str] = Field(None, example="Doe")
     bio: Optional[str] = Field(None, example="Experienced software developer specializing in web applications.")
@@ -95,16 +53,13 @@ class UserBase(BaseModel):
     _validate_nickname = validator('nickname', pre=True, allow_reuse=True)(validate_nickname)
 
     _validate_urls = validator('profile_picture_url', 'linkedin_profile_url', 'github_profile_url', pre=True, allow_reuse=True)(validate_url)
-    _validate_github_url = validator('github_profile_url', pre=True, allow_reuse=True)(github_validate_url)
-    _validate_linkedin_url = validator('linkedin_profile_url', pre=True, allow_reuse=True)(linkedin_validate_url)
-
+ 
     class Config:
         from_attributes = True
 
 class UserCreate(UserBase):
     email: EmailStr = Field(..., example="john.doe@example.com")
     password: str = Field(..., example="Secure*1234")
-    _validate_password = validator('password', pre=True, allow_reuse=True)(validate_password)
 
 class UserUpdate(UserBase):
     email: Optional[EmailStr] = Field(None, example="john.doe@example.com")
@@ -126,7 +81,7 @@ class UserResponse(UserBase):
     id: uuid.UUID = Field(..., example=uuid.uuid4())
     role: UserRole = Field(default=UserRole.AUTHENTICATED, example="AUTHENTICATED")
     email: EmailStr = Field(..., example="john.doe@example.com")
-    nickname: Optional[str] = Field(None, min_length=3, pattern=r'^[\w-]+$', example=generate_nickname())    
+    nickname: Optional[str] = Field(None, min_length=3, pattern=r'^[\w-]+$', example=random_nickname)    
     role: UserRole = Field(default=UserRole.AUTHENTICATED, example="AUTHENTICATED")
     is_professional: Optional[bool] = Field(default=False, example=True)
 
@@ -140,7 +95,7 @@ class ErrorResponse(BaseModel):
 
 class UserListResponse(BaseModel):
     items: List[UserResponse] = Field(..., example=[{
-        "id": uuid.uuid4(), "nickname": generate_nickname(), "email": "john.doe@example.com",
+        "id": uuid.uuid4(), "nickname": random_nickname, "email": "john.doe@example.com",
         "first_name": "John", "bio": "Experienced developer", "role": "AUTHENTICATED",
         "last_name": "Doe", "bio": "Experienced developer", "role": "AUTHENTICATED",
         "profile_picture_url": "https://example.com/profiles/john.jpg", 
